551ef5a55383d51579e58a93be25ade0c3378228
LizaKim0101001 <liza.kim.95@list.ru>
2022-04-14 22:00:53 +0500

    add project

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = ()=>{
+    <section className="main__header">
+        <section className="main__header-wrap">
+            <span className="main__header-logo">SomeList</span>
+            <div className="main__header-group-lnk">
+                <a href="#" className="main__header-lnk lnk-active">События</a>
+                <a href="#" className="main__header-lnk">Архив</a>
+            </div>
+        </section>
+    </section>
+};
+
+export default Header;
